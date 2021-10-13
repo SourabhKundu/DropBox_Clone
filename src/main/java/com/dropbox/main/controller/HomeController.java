95953e4a7db9aa2ff32b7617877b8d0499d11cb8
NarasimhaMount <narasimha.nandikonda.17.1@mountblue.tech>
2021-10-13 12:51:59 +0530

    added method in controller to redirect to home page

--- src/main/java/com/dropbox/main/controller/HomeController.java
+++ src/main/java/com/dropbox/main/controller/HomeController.java
@@ -1,4 +1,28 @@
 package com.dropbox.main.controller;
 
+import com.dropbox.main.model.File;
+import com.dropbox.main.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
+
+    private FileService fileService;
+
+    @Autowired
+    public HomeController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @GetMapping("/")
+    public String getHome(Model model) {
+        return "main";
+    }
 }
--- src/main/java/com/dropbox/main/service/FileService.java
+++ src/main/java/com/dropbox/main/service/FileService.java
@@ -1,5 +1,16 @@
 package com.dropbox.main.service;
 
+import com.dropbox.main.repository.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FileService {
 
+    private FileRepository fileRepository;
+
+    @Autowired
+    public FileService(FileRepository fileRepository) {
+        this.fileRepository = fileRepository;
+    }
 }
