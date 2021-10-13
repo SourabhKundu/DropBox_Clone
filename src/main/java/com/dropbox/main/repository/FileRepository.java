9ae567626ec7aaa9680984672b6f52692c0e926b
Shreya Jadhav <shreya.jadhav.17.1@mountblue.tech>
2021-10-13 13:30:03 +0530

    Edit the respository file and Extended to jpa

--- src/main/java/com/dropbox/main/repository/FileRepository.java
+++ src/main/java/com/dropbox/main/repository/FileRepository.java
@@ -1,4 +1,8 @@
 package com.dropbox.main.repository;
+import com.dropbox.main.model.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface FileRepository {
+@Repository
+public interface FileRepository extends JpaRepository<File, Integer>{
 }
