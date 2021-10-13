d75ef76af11c8ff0d8492dacc2f5cfe56455b067
NarasimhaMount <narasimha.nandikonda.17.1@mountblue.tech>
2021-10-13 11:37:36 +0530

    Created structure for dropbox project

---
+++ src/main/java/com/dropbox/main/DropBoxMainApplication.java
@@ -0,0 +1,13 @@
+package com.dropbox.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropBoxMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropBoxMainApplication.class, args);
+    }
+
+}
