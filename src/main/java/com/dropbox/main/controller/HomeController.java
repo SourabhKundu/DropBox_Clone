d87113762fde8e9c60d7f267cea37c6dbc8000c1
NarasimhaMount <narasimha.nandikonda.17.1@mountblue.tech>
2021-10-13 13:11:59 +0530

    created a method to handle upload post request

--- src/main/java/com/dropbox/main/controller/HomeController.java
+++ src/main/java/com/dropbox/main/controller/HomeController.java
@@ -6,15 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
-    private FileService fileService;
+    private final FileService fileService;
 
     @Autowired
     public HomeController(FileService fileService) {
@@ -25,4 +29,11 @@ public class HomeController {
     public String getHome(Model model) {
         return "main";
     }
+
+    @PostMapping("upload")
+    public String saveFile(@RequestParam("file") MultipartFile file) throws IOException {
+        fileService.save(file);
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/dropbox/main/service/FileService.java
+++ src/main/java/com/dropbox/main/service/FileService.java
@@ -1,8 +1,14 @@
 package com.dropbox.main.service;
 
+import com.dropbox.main.model.File;
 import com.dropbox.main.repository.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Objects;
 
 @Service
 public class FileService {
@@ -13,4 +19,6 @@ public class FileService {
     public FileService(FileRepository fileRepository) {
         this.fileRepository = fileRepository;
     }
+
+
 }
