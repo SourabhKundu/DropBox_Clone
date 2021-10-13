f4279c41b93b33257ab10646271d395fbf19a531
Shreya Jadhav <shreya.jadhav.17.1@mountblue.tech>
2021-10-13 13:10:11 +0530

    declared variables in model class

--- src/main/java/com/dropbox/main/model/File.java
+++ src/main/java/com/dropbox/main/model/File.java
@@ -1,6 +1,43 @@
 package com.dropbox.main.model;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Files")
 public class File {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "entity_id_seq"
+    )
+    @SequenceGenerator(
+            name = "entity_id_seq",
+            sequenceName = "global_id_sequence",
+            allocationSize = 1
+    )
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "data", columnDefinition = "clob")
+    private byte[] data;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
 
 
 }
