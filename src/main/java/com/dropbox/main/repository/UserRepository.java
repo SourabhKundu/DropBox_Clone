529b0c5338a0d22f1824203ad4718015fd664380
shreya_jadhav <shreya.jadhav.17.1@mountblue.tech>
2021-10-16 01:26:17 +0530

    created user repository

---
+++ src/main/java/com/dropbox/main/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.dropbox.main.repository;
+
+import com.dropbox.main.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query(value = "select u.id from users u where u.email in (?1)",nativeQuery = true)
+    public int[] findIdByEmail(Set<String> emails);
+}
---
+++ src/main/java/com/dropbox/main/service/UserService.java
@@ -0,0 +1,14 @@
+package com.dropbox.main.service;
+
+import com.dropbox.main.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class UserService {
+
+
+}
