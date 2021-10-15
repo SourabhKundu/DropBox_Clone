7ef70b1ff87b01be8eadfc8b42f797bb3a7cd76a
shreya_jadhav <shreya.jadhav.17.1@mountblue.tech>
2021-10-16 01:49:23 +0530

    created OwnerGuest repository

---
+++ src/main/java/com/dropbox/main/repository/OwnerGuestRepository.java
@@ -0,0 +1,16 @@
+package com.dropbox.main.repository;
+
+import com.dropbox.main.model.File;
+import com.dropbox.main.model.OwnerGuest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OwnerGuestRepository  extends JpaRepository<OwnerGuest, Integer> {
+
+    @Query(value = "select * from owner_guest o where o.guestid = ?1",nativeQuery = true)
+    public List<OwnerGuest> getByGuestId(int id);
+}
