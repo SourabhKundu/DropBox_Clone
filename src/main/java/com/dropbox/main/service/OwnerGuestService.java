4c8809d4a9d4e1b32d55f3c24189446f3b8e6ddc
shreya_jadhav <shreya.jadhav.17.1@mountblue.tech>
2021-10-16 01:51:06 +0530

    created and implented service for OwnerGuest

---
+++ src/main/java/com/dropbox/main/service/OwnerGuestService.java
@@ -0,0 +1,34 @@
+package com.dropbox.main.service;
+
+import com.dropbox.main.model.Notification;
+import com.dropbox.main.model.OwnerGuest;
+import com.dropbox.main.repository.OwnerGuestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class OwnerGuestService {
+
+    @Autowired
+    private OwnerGuestRepository ownerGuestRepository;
+
+    public void save(int userId, int fileId, int[] guestIds, boolean access) {
+
+        for (int guestId : guestIds) {
+            OwnerGuest ownerGuest = new OwnerGuest();
+            ownerGuest.setUserId(userId);
+            ownerGuest.setGuestId(guestId);
+            ownerGuest.setFileId(fileId);
+            ownerGuest.setAccess(access);
+            ownerGuestRepository.save(ownerGuest);
+        }
+    }
+
+    public List<OwnerGuest> findByGuestId(int id) {
+        return ownerGuestRepository.getByGuestId(id);
+    }
+}
