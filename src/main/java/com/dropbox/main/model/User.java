1b31aea435b308c39d687ebc9df7c89e03deb3df
Sourabh Kundu <sourabh.kundu.17.1@mountblue.tech>
2021-10-16 00:38:23 +0530

    Created constructor and getter setter for User model

--- src/main/java/com/dropbox/main/model/User.java
+++ src/main/java/com/dropbox/main/model/User.java
@@ -28,4 +28,41 @@ public class User {
     @Column(name = "password", nullable = false)
     private String password;
 
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.name = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) { this.password = password;
+    }
 }
