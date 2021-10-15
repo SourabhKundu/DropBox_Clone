28fa1dd8bbe950374905c066a88e4a67c7553201
Sourabh Kundu <sourabh.kundu.17.1@mountblue.tech>
2021-10-16 00:36:10 +0530

    Created new model for User table

---
+++ src/main/java/com/dropbox/main/model/User.java
@@ -0,0 +1,31 @@
+package com.dropbox.main.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "entity_id_seq"
+    )
+    @SequenceGenerator(
+            name = "entity_id_seq",
+            sequenceName = "global_id_sequence",
+            allocationSize = 1
+    )
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
