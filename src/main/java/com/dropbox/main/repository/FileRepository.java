c697de73e2b7134411086b203d2da016813923fc
NarasimhaMount <narasimha.nandikonda.17.1@mountblue.tech>
2021-10-16 19:50:57 +0530

    modified application to work in heroku

--- src/main/java/com/dropbox/main/repository/FileRepository.java
+++ src/main/java/com/dropbox/main/repository/FileRepository.java
@@ -4,11 +4,13 @@ import com.dropbox.main.model.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface FileRepository extends JpaRepository<File, Integer> {
+    @Transactional
     @Query(value = "select f.id, f.name from files f", nativeQuery = true)
     List<File> allFiles();
 }
