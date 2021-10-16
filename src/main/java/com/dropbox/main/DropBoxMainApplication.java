60e38fdd8d17c7bf948ad056830dd4ef1b592e58
NarasimhaMount <narasimha.nandikonda.17.1@mountblue.tech>
2021-10-16 16:09:57 +0530

    extended spring initializer

--- src/main/java/com/dropbox/main/DropBoxMainApplication.java
+++ src/main/java/com/dropbox/main/DropBoxMainApplication.java
@@ -2,9 +2,10 @@ package com.dropbox.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DropBoxMainApplication {
+public class DropBoxMainApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DropBoxMainApplication.class, args);
