88eb43f55eeb28e87999993ccd83c89e7083045d
NarasimhaMount <narasimha.nandikonda.17.1@mountblue.tech>
2021-10-18 00:19:20 +0530

    overriden load userdetails method

--- src/main/java/com/dropbox/main/service/UserService.java
+++ src/main/java/com/dropbox/main/service/UserService.java
@@ -5,11 +5,17 @@ import com.dropbox.main.model.Role;
 import com.dropbox.main.model.User;
 import com.dropbox.main.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -36,5 +42,19 @@ public class UserService {
                 registrationDto.getEmail(), passwordEncoder.encode(registrationDto.getPassword()),
                 List.of(new Role("ROLE_USER")));
         return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid username or password");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                mapRolesToAuthority(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthority(Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 }
