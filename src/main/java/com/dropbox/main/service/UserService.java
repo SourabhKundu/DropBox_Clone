13e24129871bf32413855ac113ccd7c09616e37b
NarasimhaMount <narasimha.nandikonda.17.1@mountblue.tech>
2021-10-18 00:16:11 +0530

    implemented a save method to save user

--- src/main/java/com/dropbox/main/service/UserService.java
+++ src/main/java/com/dropbox/main/service/UserService.java
@@ -1,8 +1,11 @@
 package com.dropbox.main.service;
 
+import com.dropbox.main.config.web.UserRegistrationDto;
+import com.dropbox.main.model.Role;
 import com.dropbox.main.model.User;
 import com.dropbox.main.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,10 +15,12 @@ import java.util.Set;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<User> getAllUsers() {
@@ -24,5 +29,12 @@ public class UserService {
 
     public int[] getIdsByEmail(Set<String> emails) {
         return userRepository.findIdByEmail(emails);
+    }
+
+    public User save(UserRegistrationDto registrationDto) {
+        User user = new User(registrationDto.getName(),
+                registrationDto.getEmail(), passwordEncoder.encode(registrationDto.getPassword()),
+                List.of(new Role("ROLE_USER")));
+        return userRepository.save(user);
     }
 }
