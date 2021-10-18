f927e70dc76e5685b7f6ab1c798e4642993dc55e
NarasimhaMount <narasimha.nandikonda.17.1@mountblue.tech>
2021-10-19 02:24:23 +0530

    added new bean to fix application error

--- src/main/java/com/dropbox/main/DropBoxMainApplication.java
+++ src/main/java/com/dropbox/main/DropBoxMainApplication.java
@@ -1,7 +1,11 @@
 package com.dropbox.main;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.unit.DataSize;
@@ -18,8 +22,16 @@ public class DropBoxMainApplication {
     @Bean
     MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
-        factory.setMaxFileSize(DataSize.ofBytes(512000000L));
-        factory.setMaxRequestSize(DataSize.ofBytes(512000000L));
+        factory.setMaxFileSize(DataSize.ofBytes(1000000000L));
+        factory.setMaxRequestSize(DataSize.ofBytes(1000000000L));
         return factory.createMultipartConfig();
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory containerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(connector ->
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1));
+        return factory;
     }
 }
