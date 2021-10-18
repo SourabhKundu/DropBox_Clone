f53f0db77ace80cadf2fb64471cd1c40e8413932
shreya_jadhav <shreya.jadhav.17.1@mountblue.tech>
2021-10-19 00:50:09 +0530

    implemented method to save notification

--- src/main/java/com/dropbox/main/service/OwnerGuestService.java
+++ src/main/java/com/dropbox/main/service/OwnerGuestService.java
@@ -5,6 +5,7 @@ import com.dropbox.main.model.Notification;
 import com.dropbox.main.model.OwnerGuest;
 import com.dropbox.main.model.User;
 import com.dropbox.main.repository.FileRepository;
+import com.dropbox.main.repository.NotificationRepository;
 import com.dropbox.main.repository.OwnerGuestRepository;
 import com.dropbox.main.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,24 +22,33 @@ public class OwnerGuestService {
     private final OwnerGuestRepository ownerGuestRepository;
     private final FileRepository fileRepository;
     private final UserRepository userRepository;
+    private final NotificationRepository notificationRepository;
 
     @Autowired
     public OwnerGuestService(OwnerGuestRepository ownerGuestRepository,
                              FileRepository fileRepository,
-                             UserRepository userRepository) {
+                             UserRepository userRepository,
+                             NotificationRepository notificationRepository) {
         this.fileRepository = fileRepository;
         this.userRepository = userRepository;
         this.ownerGuestRepository = ownerGuestRepository;
+        this.notificationRepository = notificationRepository
     }
 
     public void save(int userId, int fileId, int[] guestIds, boolean access) {
 
         for (int guestId : guestIds) {
+            Optional<File> optionalFile = fileRepository.findById(fileId);
+            Optional<User> optionalUser = userRepository.findById(guestId);
+            File file = optionalFile.get();
+            User user = optionalUser.get();
             OwnerGuest ownerGuest = new OwnerGuest();
             ownerGuest.setUserId(userId);
             ownerGuest.setGuestId(guestId);
             ownerGuest.setFileId(fileId);
             ownerGuest.setAccess(access);
+            Notification notification = new Notification(fileId,guestId, file.getName(), user.getName(), access);
+            notificationRepository.save(notification);
             ownerGuestRepository.save(ownerGuest);
         }
     }
