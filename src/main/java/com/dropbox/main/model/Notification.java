d60020134569569340bb8b5fcd5a6e0f8b0159bf
Sourabh Kundu <sourabh.kundu.17.1@mountblue.tech>
2021-10-18 23:57:24 +0530

    modified notification model class

--- src/main/java/com/dropbox/main/model/Notification.java
+++ src/main/java/com/dropbox/main/model/Notification.java
@@ -1,14 +1,42 @@
 package com.dropbox.main.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "notification")
 public class Notification {
 
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "entity_id_seq"
+    )
+    @SequenceGenerator(
+            name = "entity_id_seq",
+            sequenceName = "global_id_sequence",
+            allocationSize = 1
+    )
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "file_id")
     private int fileId;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "filename")
     private String fileName;
+
+    @Column(name = "sharedby")
     private String sharedBy;
+
+    @Column(name = "access")
     private boolean access;
 
-    public Notification(int fileId, String fileName, String sharedBy, boolean access) {
+    public Notification(int fileId, int userId, String fileName, String sharedBy, boolean access) {
         this.fileId = fileId;
+        this.userId = userId;
         this.fileName = fileName;
         this.sharedBy = sharedBy;
         this.access = access;
@@ -47,5 +75,13 @@ public class Notification {
 
     public void setAccess(boolean access) {
         this.access = access;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
